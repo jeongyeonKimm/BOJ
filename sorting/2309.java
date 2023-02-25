09fc1a28428b6c6676b7c7eca35811bcd73eb4c3
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-02-25 09:58:36 +0900

    sorting 2309

---
+++ sorting/2309.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int[] arr = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    int a = i;
+                    int b = j;
+                    for (int k = 0; k < arr.length; k++) {
+                        if (k != a && k != b) {
+                            System.out.println(arr[k]);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
