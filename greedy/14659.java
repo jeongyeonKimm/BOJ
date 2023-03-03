aadefe5a000994d70c16fb678a37dfcdcc092457
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-03-04 07:08:56 +0900

    Greedy 14659

---
+++ greedy/14659.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            arr[i] = x;
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+            max = Math.max(max, cnt);
+        }
+
+        System.out.println(max);
+    }
+}
