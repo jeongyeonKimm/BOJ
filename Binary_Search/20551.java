998df6c5fca7072b7a8f569af4721bb4142480c0
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-03-22 01:12:59 +0900

    Binary Search 20551

---
+++ Binary_Search/20551.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+
+    public static int binarySearch(int target) {
+        int left = 0;
+        int right = n - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < m; i++) {
+            int d = Integer.parseInt(br.readLine());
+            int x = binarySearch(d);
+            System.out.println(x);
+        }
+    }
+}
