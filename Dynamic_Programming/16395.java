b5a03ecfa1bd16ca2d64d30fbc928fcf1ba8537e
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-03-30 16:55:52 +0900

    Dynamic Programming 16395 update

--- Dynamic_Programming/16395.java
+++ Dynamic_Programming/16395.java
@@ -11,12 +11,10 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[n + 2][n + 2];
+        int[][] dp = new int[n + 1][n + 1];
         dp[1][1] = 1;
-        dp[2][1] = 1;
-        dp[2][2] = 1;
 
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= k; j++) {
                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
             }
