ef0f17044a58f8598c56036a5aa79cc639438f8d
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-04-13 14:16:34 +0900

    Implementation 1085

---
+++ Implementation/1085.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int minX = Math.min(x, w - x);
+        int minY = Math.min(y, h - y);
+
+        int min = Math.min(minX, minY);
+
+        System.out.println(min);
+    }
+}
