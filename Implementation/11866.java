35898c3c459c4545bbc523425c5d49221ac97058
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-05-31 01:53:04 +0900

    Implementation 11866

---
+++ Implementation/11866.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (queue.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                int x = queue.poll();
+                queue.offer(x);
+            }
+            sb.append(queue.poll() + ", ");
+        }
+        sb.append(queue.poll()).append(">");
+
+        System.out.println(sb);
+    }
+}
